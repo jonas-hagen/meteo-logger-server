cb71e7c6a473dfc857593f6e7ea74673a706c24a
Jonas Hagen <jonas.hagen@iap.unibe.ch>
2019-03-26 12:46:31 +0100

    Do not start upon install and stop services before install.

--- install.sh
+++ install.sh
@@ -2,6 +2,9 @@
 
 set -x
 
+systemctl stop meteologger.service
+systemctl stop meteoserver.service
+
 cp -n meteo.default.yml /etc/meteo.yml
 cp meteologger.service /etc/systemd/system/
 cp meteoserver.service /etc/systemd/system/
@@ -9,8 +12,8 @@ cp meteoserver.service /etc/systemd/system/
 pip3 install . --upgrade
 
 systemctl enable meteologger.service
-systemctl start meteologger.service
-
 systemctl enable meteoserver.service
-systemctl start meteoserver.service
 
+
+echo 'You can start the services with:'
+echo 'systemctl start meteologger.service meteoserver.service'
