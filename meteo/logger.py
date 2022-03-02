6d128b310fa9c7d756ad6d5a8ed0ed3d529aee74
tcpluess <tpluess@ieee.org>
2022-03-02 11:15:25 +0100

    create yearly subfolders

--- meteo/logger.py
+++ meteo/logger.py
@@ -264,6 +264,7 @@ def meteo_logger(config):
                 # Write csv
                 day = now.date()
                 path = pathlib.Path(output_dir) / str(now.year) / ('meteo_' + str(day) + '.csv')
+                path.parent.mkdir(parents=True, exist_ok=True)
                 append_csv_row(path, data)
 
                 # Store to database
